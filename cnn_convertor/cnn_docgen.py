4e41e9002aa604eb8cffbf5b561ed595fe9cbb32
LYU, Zong Hong <zonghong.lyu@dmprof.com>
2018-10-16 11:26:21 +0900

    Added DMP logo to generate documents.

--- cnn_convertor/cnn_docgen.py
+++ cnn_convertor/cnn_docgen.py
@@ -71,7 +71,7 @@ PROJECT_BRIEF          = {proj_name}" generated configuration for DMP DV700 FPGA
 # pixels and the maximum width should not exceed 200 pixels. Doxygen will copy
 # the logo to the output directory.
 
-PROJECT_LOGO           =
+PROJECT_LOGO           = ../../doc/dmpLogo.png
 
 # The OUTPUT_DIRECTORY tag is used to specify the (relative or absolute) path
 # into which the generated documentation will be written. If a relative path is
