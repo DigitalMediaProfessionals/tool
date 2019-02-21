fca61d082dcfe7ad3704e752c3a7b55a85e5eb0d
YuyaKobayashi <yuya.kobayashi@dmprof.com>
2019-02-21 17:16:45 +0900

    small change

--- cnn_convertor/fpga_layer.py
+++ cnn_convertor/fpga_layer.py
@@ -1078,12 +1078,9 @@ class FPGALayer(object):
                     need_copy = False
                     # ignore the last input node
                     for node_in in node.input_nodes[:-1]:
-                        if (len(node.output_dim) == 3 and
-                                node_in.output_dim[2] % 8 != 0):
-                            need_copy = True
-                            break
-                        if node_in.output_size !=\
-                                make_align_size(node_in.output_size):
+                        if (node_in.output_dim[-1] % 8 != 0) or
+                            (node_in.output_size !=
+                                make_align_size(node_in.output_size)):
                             need_copy = True
                             break
                     if need_copy:
